57c473cae070c8b62320f20ddaa66e8460d9413b
nslebruh <nslebrun1@gmail.com>
2023-02-11 15:25:46 +1100

    Initial commit

---
+++ app/public/Week1/test.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Sample Webpage</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello world from a PHP echo statement"
+        ?>
+    </body>
+</html>
